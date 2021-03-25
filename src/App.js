0cafadf8c85275d4c36d661fbc645249e86f3bf1
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-25 21:21:52 +0600

    added some components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Layout/Navbar';
+import Index from './Components/Layout/Index';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar></Navbar>
-      </div>
+      <Router>
+        <>
+          <Navbar></Navbar>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index}></Route>
+            </Switch>
+          </div>
+        </>
+      </Router>
     );
   }
 }
---
+++ src/Components/Layout/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Track from '../../Components/Track/Track';
+
+const Index = () => {
+  return (
+    <>
+      <Track></Track>
+    </>
+  );
+};
+
+export default Index;
